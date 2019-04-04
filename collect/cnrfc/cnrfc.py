a10801c5c8455fc489752b9f556610e58834fd12
narlesky <narlesky@mbkengineers.com>
2019-04-04 10:24:44 -0700

    Extend get_watershed method to include all regions in ensemble product zipfile downloads pages

--- collect/cnrfc/cnrfc.py
+++ collect/cnrfc/cnrfc.py
@@ -411,12 +411,38 @@ def get_watershed(cnrfc_id):
     """
     get associated hydrologic region for CNRFC forecast location
     """
-    if cnrfc_id.upper() in ['AKYC1', 'AKYC1F', 'BCTC1', 'CBAC1', 'CBAC1F', 'CBAC1L', 'FMDC1', 'FMDC1O', 'FOLC1', 'FOLC1F', 'HLLC1', 'HLLC1F', 'HLLC1L', 'HLLC1SPL', 'ICHC1', 'LNLC1', 'LNLC1F', 'MFAC1', 'MFAC1F', 'MFAC1L', 'NFDC1', 'NMFC1', 'RBBC1F', 'RBBC1SPL', 'RRGC1', 'RRGC1F', 'RRGC1L', 'RUFC1', 'RUFC1L', 'SVCC1', 'SVCC1F', 'SVCC1L', 'UNVC1', 'UNVC1F']:
-        return 'american'
+    if cnrfc_id.upper() in ['KEOO3L', 'BOYO3L', 'IRGC1L', 'BTYO3', 'SCNO3', 'CHSO3', 'WKAO3', 'WMSO3', 'KEOO3', 'BOYO3', 'IRGC1', 'YREC1', 'FTJC1', 'SEIC1', 'SBRC1', 'HAPC1', 'ONSC1', 'CEGC1', 'TRJC1', 'BURC1', 'HYMC1', 'HOOC1']:
+        return 'klamath'
+    elif cnrfc_id.upper() in ['CREC1', 'FTDC1', 'BLKC1', 'ORIC1', 'MAUC1', 'ARCC1', 'BRGC1', 'DOSC1', 'PLBC1', 'FTSC1', 'LEGC1', 'MRNC1', 'SCOC1', 'FRNC1']:
+        return 'NorthCoast'
+    elif cnrfc_id.upper() in ['NVRC1', 'LAMC1', 'UKAC1', 'HOPC1', 'CDLC1', 'HEAC1', 'WSDC1', 'GUEC1', 'SHEC1', 'APCC1', 'HOPC1L', 'CDLC1L', 'HEAC1L', 'GUEC1L']:
+        return 'RussianNapa'    
     elif cnrfc_id.upper() in ['DLTC1', 'MMCC1', 'MSSC1', 'MSSC1F', 'CNBC1', 'PITC1F', 'SHDC1', 'PLYC1', 'CWAC1', 'COTC1', 'CWCC1', 'WHSC1', 'EDCC1', 'MLMC1', 'TCRC1', 'DCVC1', 'HKCC1', 'BKCC1', 'EPRC1', 'SGEC1', 'BLBC1', 'BDBC1', 'RDBC1', 'TEHC1', 'VWBC1', 'HAMC1', 'ORFC1', 'BTCC1', 'CLSC1', 'CLAC1', 'CLUC1', 'TISC1', 'WLKC1', 'RDBC1L', 'TEHC1L', 'BDBC1L', 'RDBC1L2', 'VWBC1L', 'ORFC1L']:
         return 'UpperSacramento'
+    elif cnrfc_id.upper() in ['PLLC1', 'IIFC1', 'ANTC1', 'DVSC1', 'FHDC1', 'SCBC1', 'MFTC1', 'MRMC1', 'NFEC1', 'PLGC1', 'WBGC1', 'ORDC1', 'GYRC1', 'NBBC1', 'HLEC1', 'CFWC1', 'JKRC1', 'BWKC1', 'FOCC1', 'SUAC1', 'SOVC1', 'DMCC1', 'ROLC1I', 'DCSC1', 'JNSC1', 'OURC1', 'LOCC1', 'HCTC1', 'CFWC1O', 'DCWC1', 'NBBC1L', 'OURC1L', 'SUAC1L', 'JNSC1L', 'DCSC1L', 'LOCC1L', 'HLEC1L', 'MRYC1L', 'HLEC1LT', 'MRYC1LT', 'YUBC1L', 'OURC1R', 'JNSC1R', 'NBBC1R']:
+        return 'FeatherYuba'
+    elif cnrfc_id.upper() in ['MUPC1', 'KCVC1', 'SKPC1', 'CLKC1T', 'HOUC1', 'HCHC1', 'INVC1', 'PCGC1', 'LBEC1']:
+        return 'CachePutah'    
+    elif cnrfc_id.upper() in ['NFDC1', 'FMDC1', 'FMDC1O', 'BCTC1', 'LNLC1', 'RRGC1', 'HLLC1', 'HLLC1F', 'NMFC1', 'RUFC1', 'MFAC1', 'MFAC1F', 'UNVC1F', 'ICHC1', 'AKYC1', 'AKYC1F', 'CBAC1', 'CBAC1F', 'FOLC1', 'FOLC1F', 'UNVC1', 'SVCC1', 'MFAC1L', 'CBAC1L', 'RBBC1F', 'RBBC1SPL', 'LNLC1F', 'RRGC1L', 'RRGC1F', 'RUFC1L', 'SVCC1F', 'SVCC1L', 'HLLC1L', 'HLLC1SPL']:
+        return 'american'
+    elif cnrfc_id.upper() in ['SAMC1', 'SACC1', 'VONC1', 'FMWC1', 'DRMC1', 'RCVC1', 'FMWC1L', 'SACC1L', 'SAMC1L', 'NCOC1L']:
+        return 'LowerSacramento'
+    elif cnrfc_id.upper() in ['LWDC1', 'SNRC1', 'NBYC1', 'NACC1', 'PRBC1', 'RDRC1', 'BSRC1', 'PIIC1', 'TESC1', 'HOSC1', 'PHOC1', 'AROC1', 'BTEC1', 'COYC1', 'ANDC1', 'CYTC1', 'CYEC1', 'CMIC1', 'LEXC1', 'ALRC1', 'GUAC1', 'CADC1', 'ANOC1', 'LVKC1', 'AHOC1', 'CVQC1', 'MPTC1', 'LRZC1', 'SFCC1', 'GUDC1', 'GSJC1']:
+        return 'CentralCoast'
+    elif cnrfc_id.upper() in ['TWDC1', 'SSQC1', 'GARC1', 'LLYC1', 'CCHC1', 'SLUC1', 'VRVC1', 'CLLC1', 'EFBC1', 'CSKC1', 'PYMC1', 'LKPC1', 'YDRC1', 'SLOC1', 'TEKC1', 'VLKC1', 'MUTC1', 'SMHC1', 'SHRC1', 'TIMC1', 'SREC1', 'SRWC1', 'KNBC1', 'YTLC1', 'MWXC1', 'TSLC1', 'ADOC1', 'SVWC1', 'DKHC1', 'WFMC1', 'MVDC1O', 'MVVC1', 'HAWC1', 'ELPC1', 'SVIC1', 'FSNC1', 'CYBC1', 'HSAC1']:
+        return 'SouthernCalifornia'
+    elif cnrfc_id.upper() in ['KKVC1', 'SKRC1', 'ISAC1', 'SCSC1', 'KTRC1', 'TMDC1', 'PFTC1', 'DLMC1', 'MLPC1']:
+        return 'Tulare'
+    elif cnrfc_id.upper() in ['FRAC1', 'HIDC1', 'BHNC1', 'MPAC1', 'OWCC1', 'BNCC1', 'BCKC1', 'MEEC1', 'HPIC1', 'POHC1', 'EXQC1', 'HETC1', 'LNRC1', 'CHVC1F', 'NDPC1', 'NDVC1I', 'NSWC1I', 'AVYC1', 'NMSC1', 'LTDC1', 'DSNC1', 'DCMC1', 'STVC1', 'MDSC1', 'OBBC1', 'RIPC1', 'NWMC1', 'CRDC1', 'PATC1', 'VNSC1']:
+        return 'SanJoaquin'
+    elif cnrfc_id.upper() in ['CMPC1', 'NHGC1', 'MSGC1', 'FRGC1', 'EDOC1', 'SOSC1', 'MHBC1', 'MCNC1']:
+        return 'N_SanJoaquin'
+    elif cnrfc_id.upper() in ['SUSC1', 'SCRN2', 'SCWN2', 'WOOC1', 'CEMC1', 'GRDN2', 'STWN2', 'FTCN2', 'WWBC1', 'PSRC1', 'STPC1', 'STPC1F', 'BCAC1', 'BCAC1F', 'FARC1', 'ILAC1', 'LWON2', 'TRRN2', 'VISN2', 'BPRC1', 'SGNC1', 'TAHC1', 'TRCC1', 'DNRC1', 'MTSC1']:
+        return 'EastSierra'
+    elif cnrfc_id.upper() in ['MBON2', 'MHSN2', 'DVGN2', 'HREN2', 'DIXN2', 'HRCN2', 'PALN2', 'ROCN2', 'HBMN2', 'CMSN2', 'HRIN2', 'LHPN2', 'MARN2', 'MDCN2']:
+        return 'Humboldt'
     else:
-        return None
+        raise ValueError('cnrfc_id not recognized.')
 
 
 def default_date_string(date_string):
@@ -450,6 +476,11 @@ if __name__ == '__main__':
                   'Oroville': 'ORDC1',
                   'Pine Flat': 'PNFC1',
                   'Shasta': 'SHDC1'}
+
+    for watershed in ['klamath', 'NorthCoast', 'RussianNapa', 'UpperSacramento', 'FeatherYuba', 'CachePutah', 'american', 'LowerSacramento', 'CentralCoast', 'SouthernCalifornia', 'Tulare', 'SanJoaquin', 'N_SanJoaquin',  'EastSierra',  'Humboldt']:
+        print('*'*88)
+        print(watershed)
+        print(get_deterministic_forecast_watershed(watershed, '2019040412')['info']['issue_time'])
 
     # pprint(get_deterministic_forecast('SHDC1', truncate_historical=False)['info'])
 
