1df5e09629aba4106ec0b7093cdd20c79c5430b5
narlesky <narlesky@mbkengineers.com>
2019-04-04 14:10:16 -0700

    Add some tests; add duration validator to CNRFC scrapers; update units handling for CNRFC scrapers

--- collect/cnrfc/cnrfc.py
+++ collect/cnrfc/cnrfc.py
@@ -136,7 +136,7 @@ def get_deterministic_forecast(cnrfc_id, truncate_historical=False):
     return {'data': df, 'info': info}
 
 
-def get_deterministic_forecast_watershed(watershed, date_string, cnrfc_id=None):
+def get_deterministic_forecast_watershed(watershed, date_string, acre_feet=False, cnrfc_id=None):
     """
     from: https://www.cnrfc.noaa.gov/deterministicHourlyProductCSV.php
     https://www.cnrfc.noaa.gov/csv/2019040318_american_csv_export.zip
@@ -183,9 +183,15 @@ def get_deterministic_forecast_watershed(watershed, date_string, cnrfc_id=None):
     else:
         columns = df.columns
     
-    # convert kcfs/day to acre-feet
-    df = df[columns] * 1000.0 * ( 3600 * 24 / 43560.0 )
+    # convert kcfs to cfs
+    df = df[columns] * 1000.0
     units = 'cfs'
+
+    # convert cfs/hour to acre-feet
+    if acre_feet:
+        if duration == 'hourly':
+            df = df * ( 3600 / 43560.0 )
+        units = 'acre-feet'
 
     # clean up
     zipdata.close()
@@ -210,6 +216,9 @@ def get_forecast_meta_deterministic(cnrfc_id, first_ordinate=False):
     url = 'https://www.cnrfc.noaa.gov/restricted/graphicalRVF_tabular.php?id={0}'.format(cnrfc_id)
     basic_auth = requests.auth.HTTPBasicAuth(os.getenv('CNRFC_USER'), os.getenv('CNRFC_PASSWORD'))
     content = requests.get(url, auth=basic_auth).content
+
+    print(url)
+    print('%')
 
     # parse HTML content
     soup = BeautifulSoup(content, 'lxml')
@@ -239,20 +248,14 @@ def get_ensemble_forecast(cnrfc_id, duration, acre_feet=False, pdt_convert=False
 
     ported from SAFCA Portal project
     possibly CNRFC is labeling GMT when it's actually already in PDT/PST??? - 13Feb2019
-    date_string = time_issued.strftime('%Y%m%d_%H%M')
     """
 
     # default ensemble forecast units    
     units = 'kcfs'
 
     # validate duration
-    if duration[0].upper() == 'H':
-        duration = 'hourly'
-    elif duration[0].upper() == 'D':
-        duration = 'daily'
-    else:
-        raise ValueError('<duration> must be one of daily, hourly')
-
+    duration = validate_duration(duration)
+    
     # get issue time of most recent hourly inflow forecast (no support for daily yet)
     date_string = default_date_string(None)
     time_issued = get_watershed_forecast_issue_time(duration, get_watershed(cnrfc_id), date_string)
@@ -307,7 +310,7 @@ def get_ensemble_forecast(cnrfc_id, duration, acre_feet=False, pdt_convert=False
                                  'issue_time': time_issued.strftime('%Y-%m-%d %H:%M')}}
 
 
-def get_ensemble_forecast_watershed(watershed, duration, date_string, cnrfc_id=None):
+def get_ensemble_forecast_watershed(watershed, duration, date_string, acre_feet=False, cnrfc_id=None):
     """
     from: get_watershed_ensemble_issue_time
           get_watershed_ensemble_daily
@@ -333,28 +336,22 @@ def get_ensemble_forecast_watershed(watershed, duration, date_string, cnrfc_id=N
     content = session.get(url, verify=False).content
 
     # store content in memory and parse zipped file
-    zipdata = io.BytesIO(content)
-    zip_ref = zipfile.ZipFile(zipdata)
+    with io.BytesIO(content) as zipdata:
 
-    # extract CSV from zip object
-    csvdata = io.BytesIO(zip_ref.read(filename.replace('.zip', '.csv')))       
-    zip_ref.close()
+        with zipfile.ZipFile(zipdata) as zip_ref:
 
-    # get date/time stamp from ensemble download page
-    try:
-        time_issued = get_watershed_forecast_issue_time(duration, watershed, date_string)
-    except:
-        time_issued = UTC.localize(dt.datetime.strptime(date_string, '%Y%m%d12')).strftime('%Y-%m-%d 12:00')
+            # extract CSV from zip object
+            with io.BytesIO(zip_ref.read(filename.replace('.zip', '.csv'))) as csvdata:
 
-    # parse forecast data from CSV
-    df = pd.read_csv(csvdata, 
-                     header=0, 
-                     skiprows=[1,], 
-                     nrows=60, 
-                     parse_dates=True, 
-                     index_col=0,
-                     float_precision='high',
-                     dtype={'GMT': str})#, 'FOLC1': float})
+                # parse forecast data from CSV
+                df = pd.read_csv(csvdata, 
+                                 header=0, 
+                                 skiprows=[1,], 
+                                 nrows=60, 
+                                 parse_dates=True, 
+                                 index_col=0,
+                                 float_precision='high',
+                                 dtype={'GMT': str})
 
     # filter watershed for single forecast point ensemble
     if cnrfc_id is not None:
@@ -362,17 +359,27 @@ def get_ensemble_forecast_watershed(watershed, duration, date_string, cnrfc_id=N
     else:
         columns = df.columns
     
-    # convert kcfs/day to acre-feet
-    df = df[columns] * 1000.0 * ( 3600 * 24 / 43560.0 )
+    # convert kcfs to cfs
+    df = df[columns] * 1000.0
     units = 'cfs'
 
-    # clean up
-    zipdata.close()
-    csvdata.close()
+    # convert cfs to acre-feet
+    if acre_feet:
+        if duration == 'hourly':
+            df = df * ( 3600 / 43560.0 )
+        elif duration == 'daily':
+            df = df * (24 * 3600 / 43560.0 )
+        units = 'acre-feet'
+
+    # get date/time stamp from ensemble download page
+    try:
+        time_issued = get_watershed_forecast_issue_time(duration, watershed, date_string)
+    except:
+        time_issued = UTC.localize(dt.datetime.strptime(date_string, '%Y%m%d12')).strftime('%Y-%m-%d 12:00')
 
     return {'data': df, 'info': {'url': url, 
                                  'watershed': watershed, 
-                                 'issue_time': issue_time,
+                                 'issue_time': time_issued,
                                  'units': units, 
                                  'duration': duration}}
 
@@ -381,16 +388,16 @@ def get_watershed_forecast_issue_time(duration, watershed, date_string=None, det
     """
     get "last modified" date/time stamp from CNRFC watershed ensemble product table
     """
-    if duration[0].upper() == 'D':
+    duration = validate_duration(duration)
+
+    if duration == 'daily':
         #" only on the 12"
         date_string = date_string[:-2] + '12'
         url = 'https://www.cnrfc.noaa.gov/ensembleProductCSV.php'
-        duration = 'daily'
         file_name = '{0}_{1}_hefs_csv_{2}.zip'
     
-    elif duration[0].upper() == 'H':
+    elif duration == 'hourly':
         url = 'https://www.cnrfc.noaa.gov/ensembleHourlyProductCSV.php'
-        duration = 'hourly'
         file_name = '{0}_{1}_hefs_csv_{2}.zip'
     
     if deterministic:
@@ -411,36 +418,25 @@ def get_watershed(cnrfc_id):
     """
     get associated hydrologic region for CNRFC forecast location
     """
-    if cnrfc_id.upper() in ['KEOO3L', 'BOYO3L', 'IRGC1L', 'BTYO3', 'SCNO3', 'CHSO3', 'WKAO3', 'WMSO3', 'KEOO3', 'BOYO3', 'IRGC1', 'YREC1', 'FTJC1', 'SEIC1', 'SBRC1', 'HAPC1', 'ONSC1', 'CEGC1', 'TRJC1', 'BURC1', 'HYMC1', 'HOOC1']:
-        return 'klamath'
-    elif cnrfc_id.upper() in ['CREC1', 'FTDC1', 'BLKC1', 'ORIC1', 'MAUC1', 'ARCC1', 'BRGC1', 'DOSC1', 'PLBC1', 'FTSC1', 'LEGC1', 'MRNC1', 'SCOC1', 'FRNC1']:
-        return 'NorthCoast'
-    elif cnrfc_id.upper() in ['NVRC1', 'LAMC1', 'UKAC1', 'HOPC1', 'CDLC1', 'HEAC1', 'WSDC1', 'GUEC1', 'SHEC1', 'APCC1', 'HOPC1L', 'CDLC1L', 'HEAC1L', 'GUEC1L']:
-        return 'RussianNapa'    
-    elif cnrfc_id.upper() in ['DLTC1', 'MMCC1', 'MSSC1', 'MSSC1F', 'CNBC1', 'PITC1F', 'SHDC1', 'PLYC1', 'CWAC1', 'COTC1', 'CWCC1', 'WHSC1', 'EDCC1', 'MLMC1', 'TCRC1', 'DCVC1', 'HKCC1', 'BKCC1', 'EPRC1', 'SGEC1', 'BLBC1', 'BDBC1', 'RDBC1', 'TEHC1', 'VWBC1', 'HAMC1', 'ORFC1', 'BTCC1', 'CLSC1', 'CLAC1', 'CLUC1', 'TISC1', 'WLKC1', 'RDBC1L', 'TEHC1L', 'BDBC1L', 'RDBC1L2', 'VWBC1L', 'ORFC1L']:
-        return 'UpperSacramento'
-    elif cnrfc_id.upper() in ['PLLC1', 'IIFC1', 'ANTC1', 'DVSC1', 'FHDC1', 'SCBC1', 'MFTC1', 'MRMC1', 'NFEC1', 'PLGC1', 'WBGC1', 'ORDC1', 'GYRC1', 'NBBC1', 'HLEC1', 'CFWC1', 'JKRC1', 'BWKC1', 'FOCC1', 'SUAC1', 'SOVC1', 'DMCC1', 'ROLC1I', 'DCSC1', 'JNSC1', 'OURC1', 'LOCC1', 'HCTC1', 'CFWC1O', 'DCWC1', 'NBBC1L', 'OURC1L', 'SUAC1L', 'JNSC1L', 'DCSC1L', 'LOCC1L', 'HLEC1L', 'MRYC1L', 'HLEC1LT', 'MRYC1LT', 'YUBC1L', 'OURC1R', 'JNSC1R', 'NBBC1R']:
-        return 'FeatherYuba'
-    elif cnrfc_id.upper() in ['MUPC1', 'KCVC1', 'SKPC1', 'CLKC1T', 'HOUC1', 'HCHC1', 'INVC1', 'PCGC1', 'LBEC1']:
-        return 'CachePutah'    
-    elif cnrfc_id.upper() in ['NFDC1', 'FMDC1', 'FMDC1O', 'BCTC1', 'LNLC1', 'RRGC1', 'HLLC1', 'HLLC1F', 'NMFC1', 'RUFC1', 'MFAC1', 'MFAC1F', 'UNVC1F', 'ICHC1', 'AKYC1', 'AKYC1F', 'CBAC1', 'CBAC1F', 'FOLC1', 'FOLC1F', 'UNVC1', 'SVCC1', 'MFAC1L', 'CBAC1L', 'RBBC1F', 'RBBC1SPL', 'LNLC1F', 'RRGC1L', 'RRGC1F', 'RUFC1L', 'SVCC1F', 'SVCC1L', 'HLLC1L', 'HLLC1SPL']:
-        return 'american'
-    elif cnrfc_id.upper() in ['SAMC1', 'SACC1', 'VONC1', 'FMWC1', 'DRMC1', 'RCVC1', 'FMWC1L', 'SACC1L', 'SAMC1L', 'NCOC1L']:
-        return 'LowerSacramento'
-    elif cnrfc_id.upper() in ['LWDC1', 'SNRC1', 'NBYC1', 'NACC1', 'PRBC1', 'RDRC1', 'BSRC1', 'PIIC1', 'TESC1', 'HOSC1', 'PHOC1', 'AROC1', 'BTEC1', 'COYC1', 'ANDC1', 'CYTC1', 'CYEC1', 'CMIC1', 'LEXC1', 'ALRC1', 'GUAC1', 'CADC1', 'ANOC1', 'LVKC1', 'AHOC1', 'CVQC1', 'MPTC1', 'LRZC1', 'SFCC1', 'GUDC1', 'GSJC1']:
-        return 'CentralCoast'
-    elif cnrfc_id.upper() in ['TWDC1', 'SSQC1', 'GARC1', 'LLYC1', 'CCHC1', 'SLUC1', 'VRVC1', 'CLLC1', 'EFBC1', 'CSKC1', 'PYMC1', 'LKPC1', 'YDRC1', 'SLOC1', 'TEKC1', 'VLKC1', 'MUTC1', 'SMHC1', 'SHRC1', 'TIMC1', 'SREC1', 'SRWC1', 'KNBC1', 'YTLC1', 'MWXC1', 'TSLC1', 'ADOC1', 'SVWC1', 'DKHC1', 'WFMC1', 'MVDC1O', 'MVVC1', 'HAWC1', 'ELPC1', 'SVIC1', 'FSNC1', 'CYBC1', 'HSAC1']:
-        return 'SouthernCalifornia'
-    elif cnrfc_id.upper() in ['KKVC1', 'SKRC1', 'ISAC1', 'SCSC1', 'KTRC1', 'TMDC1', 'PFTC1', 'DLMC1', 'MLPC1']:
-        return 'Tulare'
-    elif cnrfc_id.upper() in ['FRAC1', 'HIDC1', 'BHNC1', 'MPAC1', 'OWCC1', 'BNCC1', 'BCKC1', 'MEEC1', 'HPIC1', 'POHC1', 'EXQC1', 'HETC1', 'LNRC1', 'CHVC1F', 'NDPC1', 'NDVC1I', 'NSWC1I', 'AVYC1', 'NMSC1', 'LTDC1', 'DSNC1', 'DCMC1', 'STVC1', 'MDSC1', 'OBBC1', 'RIPC1', 'NWMC1', 'CRDC1', 'PATC1', 'VNSC1']:
-        return 'SanJoaquin'
-    elif cnrfc_id.upper() in ['CMPC1', 'NHGC1', 'MSGC1', 'FRGC1', 'EDOC1', 'SOSC1', 'MHBC1', 'MCNC1']:
-        return 'N_SanJoaquin'
-    elif cnrfc_id.upper() in ['SUSC1', 'SCRN2', 'SCWN2', 'WOOC1', 'CEMC1', 'GRDN2', 'STWN2', 'FTCN2', 'WWBC1', 'PSRC1', 'STPC1', 'STPC1F', 'BCAC1', 'BCAC1F', 'FARC1', 'ILAC1', 'LWON2', 'TRRN2', 'VISN2', 'BPRC1', 'SGNC1', 'TAHC1', 'TRCC1', 'DNRC1', 'MTSC1']:
-        return 'EastSierra'
-    elif cnrfc_id.upper() in ['MBON2', 'MHSN2', 'DVGN2', 'HREN2', 'DIXN2', 'HRCN2', 'PALN2', 'ROCN2', 'HBMN2', 'CMSN2', 'HRIN2', 'LHPN2', 'MARN2', 'MDCN2']:
-        return 'Humboldt'
+    watersheds = {'klamath': ['KEOO3L', 'BOYO3L', 'IRGC1L', 'BTYO3', 'SCNO3', 'CHSO3', 'WKAO3', 'WMSO3', 'KEOO3', 'BOYO3', 'IRGC1', 'YREC1', 'FTJC1', 'SEIC1', 'SBRC1', 'HAPC1', 'ONSC1', 'CEGC1', 'TRJC1', 'BURC1', 'HYMC1', 'HOOC1'], 
+                  'NorthCoast': ['CREC1', 'FTDC1', 'BLKC1', 'ORIC1', 'MAUC1', 'ARCC1', 'BRGC1', 'DOSC1', 'PLBC1', 'FTSC1', 'LEGC1', 'MRNC1', 'SCOC1', 'FRNC1'], 
+                  'RussianNapa' : ['NVRC1', 'LAMC1', 'UKAC1', 'HOPC1', 'CDLC1', 'HEAC1', 'WSDC1', 'GUEC1', 'SHEC1', 'APCC1', 'HOPC1L', 'CDLC1L', 'HEAC1L', 'GUEC1L'], 
+                  'UpperSacramento': ['DLTC1', 'MMCC1', 'MSSC1', 'MSSC1F', 'CNBC1', 'PITC1F', 'SHDC1', 'PLYC1', 'CWAC1', 'COTC1', 'CWCC1', 'WHSC1', 'EDCC1', 'MLMC1', 'TCRC1', 'DCVC1', 'HKCC1', 'BKCC1', 'EPRC1', 'SGEC1', 'BLBC1', 'BDBC1', 'RDBC1', 'TEHC1', 'VWBC1', 'HAMC1', 'ORFC1', 'BTCC1', 'CLSC1', 'CLAC1', 'CLUC1', 'TISC1', 'WLKC1', 'RDBC1L', 'TEHC1L', 'BDBC1L', 'RDBC1L2', 'VWBC1L', 'ORFC1L'], 
+                  'FeatherYuba': ['PLLC1', 'IIFC1', 'ANTC1', 'DVSC1', 'FHDC1', 'SCBC1', 'MFTC1', 'MRMC1', 'NFEC1', 'PLGC1', 'WBGC1', 'ORDC1', 'GYRC1', 'NBBC1', 'HLEC1', 'CFWC1', 'JKRC1', 'BWKC1', 'FOCC1', 'SUAC1', 'SOVC1', 'DMCC1', 'ROLC1I', 'DCSC1', 'JNSC1', 'OURC1', 'LOCC1', 'HCTC1', 'CFWC1O', 'DCWC1', 'NBBC1L', 'OURC1L', 'SUAC1L', 'JNSC1L', 'DCSC1L', 'LOCC1L', 'HLEC1L', 'MRYC1L', 'HLEC1LT', 'MRYC1LT', 'YUBC1L', 'OURC1R', 'JNSC1R', 'NBBC1R'], 
+                  'CachePutah' : ['MUPC1', 'KCVC1', 'SKPC1', 'CLKC1T', 'HOUC1', 'HCHC1', 'INVC1', 'PCGC1', 'LBEC1'], 
+                  'american': ['NFDC1', 'FMDC1', 'FMDC1O', 'BCTC1', 'LNLC1', 'RRGC1', 'HLLC1', 'HLLC1F', 'NMFC1', 'RUFC1', 'MFAC1', 'MFAC1F', 'UNVC1F', 'ICHC1', 'AKYC1', 'AKYC1F', 'CBAC1', 'CBAC1F', 'FOLC1', 'FOLC1F', 'UNVC1', 'SVCC1', 'MFAC1L', 'CBAC1L', 'RBBC1F', 'RBBC1SPL', 'LNLC1F', 'RRGC1L', 'RRGC1F', 'RUFC1L', 'SVCC1F', 'SVCC1L', 'HLLC1L', 'HLLC1SPL'], 
+                  'LowerSacramento': ['SAMC1', 'SACC1', 'VONC1', 'FMWC1', 'DRMC1', 'RCVC1', 'FMWC1L', 'SACC1L', 'SAMC1L', 'NCOC1L'], 
+                  'CentralCoast': ['LWDC1', 'SNRC1', 'NBYC1', 'NACC1', 'PRBC1', 'RDRC1', 'BSRC1', 'PIIC1', 'TESC1', 'HOSC1', 'PHOC1', 'AROC1', 'BTEC1', 'COYC1', 'ANDC1', 'CYTC1', 'CYEC1', 'CMIC1', 'LEXC1', 'ALRC1', 'GUAC1', 'CADC1', 'ANOC1', 'LVKC1', 'AHOC1', 'CVQC1', 'MPTC1', 'LRZC1', 'SFCC1', 'GUDC1', 'GSJC1'], 
+                  'SouthernCalifornia': ['TWDC1', 'SSQC1', 'GARC1', 'LLYC1', 'CCHC1', 'SLUC1', 'VRVC1', 'CLLC1', 'EFBC1', 'CSKC1', 'PYMC1', 'LKPC1', 'YDRC1', 'SLOC1', 'TEKC1', 'VLKC1', 'MUTC1', 'SMHC1', 'SHRC1', 'TIMC1', 'SREC1', 'SRWC1', 'KNBC1', 'YTLC1', 'MWXC1', 'TSLC1', 'ADOC1', 'SVWC1', 'DKHC1', 'WFMC1', 'MVDC1O', 'MVVC1', 'HAWC1', 'ELPC1', 'SVIC1', 'FSNC1', 'CYBC1', 'HSAC1'], 
+                  'Tulare': ['KKVC1', 'SKRC1', 'ISAC1', 'SCSC1', 'KTRC1', 'TMDC1', 'PFTC1', 'DLMC1', 'MLPC1'], 
+                  'SanJoaquin': ['FRAC1', 'HIDC1', 'BHNC1', 'MPAC1', 'OWCC1', 'BNCC1', 'BCKC1', 'MEEC1', 'HPIC1', 'POHC1', 'EXQC1', 'HETC1', 'LNRC1', 'CHVC1F', 'NDPC1', 'NDVC1I', 'NSWC1I', 'AVYC1', 'NMSC1', 'LTDC1', 'DSNC1', 'DCMC1', 'STVC1', 'MDSC1', 'OBBC1', 'RIPC1', 'NWMC1', 'CRDC1', 'PATC1', 'VNSC1'], 
+                  'N_SanJoaquin': ['CMPC1', 'NHGC1', 'MSGC1', 'FRGC1', 'EDOC1', 'SOSC1', 'MHBC1', 'MCNC1'], 
+                  'EastSierra': ['SUSC1', 'SCRN2', 'SCWN2', 'WOOC1', 'CEMC1', 'GRDN2', 'STWN2', 'FTCN2', 'WWBC1', 'PSRC1', 'STPC1', 'STPC1F', 'BCAC1', 'BCAC1F', 'FARC1', 'ILAC1', 'LWON2', 'TRRN2', 'VISN2', 'BPRC1', 'SGNC1', 'TAHC1', 'TRCC1', 'DNRC1', 'MTSC1'], 
+                  'Humboldt': ['MBON2', 'MHSN2', 'DVGN2', 'HREN2', 'DIXN2', 'HRCN2', 'PALN2', 'ROCN2', 'HBMN2', 'CMSN2', 'HRIN2', 'LHPN2', 'MARN2', 'MDCN2']}
+
+    for key, value in watersheds.items():
+        if cnrfc_id.upper() in value:
+            return key
     else:
         raise ValueError('cnrfc_id not recognized.')
 
@@ -468,23 +464,32 @@ def get_ensemble_first_forecast_ordinate(url):
     return df.index.tolist()[0].to_pydatetime()
 
 
+def validate_duration(duration):
+    if duration[0].upper() == 'D':
+        return 'daily'
+    elif duration[0].upper() == 'H':
+        return 'hourly'
+    else:
+        raise ValueError('<duration> must be one of daily, hourly')
+
+
 if __name__ == '__main__':
 
-    # from pprint import pprint
+    from pprint import pprint
     RESERVOIRS = {'Folsom': 'FOLC1',
                   'New Bullards Bar': 'NBBC1',
                   'Oroville': 'ORDC1',
                   'Pine Flat': 'PNFC1',
                   'Shasta': 'SHDC1'}
 
-    for watershed in ['klamath', 'NorthCoast', 'RussianNapa', 'UpperSacramento', 'FeatherYuba', 'CachePutah', 'american', 'LowerSacramento', 'CentralCoast', 'SouthernCalifornia', 'Tulare', 'SanJoaquin', 'N_SanJoaquin',  'EastSierra',  'Humboldt']:
-        print('*'*88)
-        print(watershed)
-        print(get_deterministic_forecast_watershed(watershed, '2019040412')['info']['issue_time'])
+    # for watershed in ['klamath']:
+    #     print('*'*88)
+    #     print(watershed)
+        # print(get_deterministic_forecast_watershed(watershed, '2019040412', 'LLLL')['data'].head())
 
-    # pprint(get_deterministic_forecast('SHDC1', truncate_historical=False)['info'])
+    pprint(get_deterministic_forecast('FOLC1', truncate_historical=False)['data'])
 
-    # print(get_ensemble_forecast('SHDC1', 'd')['data'].head())
+    # pprint(get_ensemble_forecast('SHDC1', 'h')['data'].head())
 
     # print(get_deterministic_forecast_watershed('american', None)['info'])
 
