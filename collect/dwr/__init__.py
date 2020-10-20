dfd6315e04e2910ceb29aa1366758d67bc9db15a
narlesky <narlesky@mbkengineers.com>
2020-10-20 12:20:18 -0700

    Temporarily remove direct refs to selenium for problematic installations

--- collect/dwr/__init__.py
+++ collect/dwr/__init__.py
@@ -6,7 +6,7 @@ access CDEC gage data, forecasts and water supply info
 # -*- coding: utf-8 -*-
 
 from .b120 import *
-from .casgem import *
+# from .casgem import *
 from .cawdl import *
 from .cdec import *
 from .wsi import *
