70e912ee7bf5e2697849153eabea2073dc90ddf1
ohalexander <alexander@mbkengineers.com>
2021-05-24 08:45:10 -0700

    Update to PDF reading library

--- collect/dwr/delta_conditions.py
+++ collect/dwr/delta_conditions.py
@@ -5,13 +5,13 @@ access DWR Delta Conditions PDF
 """
 # -*- coding: utf-8 -*-
 import datetime as dt
-import camelot
 
 import io
 import re
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+from pdfminer.high_level import extract_text
 
 
 def df_cleanup(df):
@@ -24,16 +24,66 @@ def df_cleanup(df):
 
 if __name__ == "__main__":
 
-    pdf_path = 'https://water.ca.gov/-/media/DWR-Website/Web-Pages/Programs/State-Water-Project/Operations-And-Maintenance/Files/Operations-Control-Office/Delta-Status-And-Operations/Delta-Operations-Daily-Summary.pdf'
+    url = 'https://water.ca.gov/-/media/DWR-Website/Web-Pages/Programs/State-Water-Project/Operations-And-Maintenance/Files/Operations-Control-Office/Delta-Status-And-Operations/Delta-Operations-Daily-Summary.pdf'
 
-    tables = camelot.read_pdf(pdf_path, flavor='stream',  strip_text=' .\n')
+    response = requests.get(url)
+    result = extract_text(io.BytesIO(response.content))
 
-    # Read the first page and clean data
-    first = tables[0].df
-    first = first.drop(first.index[0:3], axis=0)
-    print(first)
+    # # in-memory file buffer
+    # with io.StringIO(result) as buf:
 
-    first = df_cleanup(first)
-    print(first)
+    #     # parse fixed-width text-formatted table
+    #     df = pd.read_fwf(buf, 
+    #                      header=[0], 
+    #                      skiprows=[0, 1, 2, 3, 4, 5], 
+    #                      na_values=['<i>Missing</i>', 'Missing']
+    #                      )
+    #     # df = pd.read_table(buf)
 
-    # print(first.to_csv())
+    # # print(df)
+    # df.columns = ['Column']
+
+    # row = 'Clifton Court Inflow  ='
+
+    # inflow = df.loc[df['Column']==row]
+    # print(inflow)
+
+    import PyPDF2
+    # Creating a pdf file object
+    pdfFileObj = open(result,'rb')
+    # Creating a pdf reader object
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    # Getting number of pages in pdf file
+    pages = pdfReader.numPages
+    # Loop for reading all the Pages
+    for i in range(pages):
+        # Creating a page object
+        pageObj = pdfReader.getPage(i)
+        # Printing Page Number
+        print("Page No: ",i)
+        # Extracting text from page
+        # And splitting it into chunks of lines
+        text = pageObj.extractText().split("  ")
+        # Finally the lines are stored into list
+        # For iterating over list a loop is used
+        for i in range(len(text)):
+                # Printing the line
+                # Lines are seprated using "\n"
+                print(text[i],end="\n\n")
+        # For Seprating the Pages
+        print()
+    # closing the pdf file object
+    pdfFileObj.close()
+
+
+    # tables = camelot.read_pdf(url, flavor='stream',  strip_text=' .\n')
+
+    # # Read the first page and clean data
+    # first = tables[0].df
+    # first = first.drop(first.index[0:3], axis=0)
+    # print(first)
+
+    # first = df_cleanup(first)
+    # print(first)
+
+    # # print(first.to_csv())
