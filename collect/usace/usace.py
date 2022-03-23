243edb837a714d9e8cc9a104c5b734d0ab36465f
Carly Narlesky <narlesky@mbkengineers.com>
2022-03-23 12:03:46 -0700

    Add wcds.get_release_report function for retrieving reservoir change orders from WCDS site

--- collect/usace/usace.py
+++
@@ -1,8 +0,0 @@
-"""
-collect.usace.usace
-============================================================
-download release change data reports
-"""
-# -*- coding: utf-8 -*-
-
-# http://www.spk-wc.usace.army.mil/reports/release_changes.html
