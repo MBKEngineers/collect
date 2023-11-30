32455fe020ec88d1a33b5cbdea15f3fe27321ba5
Carly Narlesky <narlesky@mbkengineers.com>
2023-11-30 15:33:13 -0800

    Update support for all continuous data database resources

--- collect/dwr/__init__.py
+++ collect/dwr/__init__.py
@@ -4,4 +4,4 @@ collect.dwr
 access DWR gage data, forecasts and water supply info
 """
 # -*- coding: utf-8 -*-
-from . import b120, cdec, swp, wsi
+from . import b120, cdec, cawdl, swp, wsi
--- collect/dwr/cawdl/cawdl.py
+++ collect/dwr/cawdl/cawdl.py
@@ -2,60 +2,46 @@
 collect.dwr.cawdl.cawdl
 ============================================================
 access CA Water Data Library surface water and well data
-
-Stations info
-https://data.cnra.ca.gov/dataset/fcba3a88-a359-4a71-a58c-6b0ff8fdc53f/resource/c2b08f48-acfd-4a5b-9799-0f3e07d83192/download/stations.csv
-https://data.cnra.ca.gov/dataset/618c73fe-b28c-4399-a824-43d0278fe974/resource/03967113-1556-4100-af2c-b16a4d41b9d0/download/gwl-stations.csv
-https://data.cnra.ca.gov/dataset/618c73fe-b28c-4399-a824-43d0278fe974/resource/16f256f8-35a4-4cab-ae02-399a2914c282/download/gwl-monthly.csv
-https://data.cnra.ca.gov/dataset/618c73fe-b28c-4399-a824-43d0278fe974/resource/84e02633-00ca-47e8-97ec-c0093313ddcd/download/gwl-daily.csv
-
-Continuous data links
-Older (01N04E36Q001M)
-https://wdlstorageaccount.blob.core.windows.net/continuous/01N04E36Q001M/por/01N04E36Q001M_Groundwater_Level_Below_Ground_Surface_Raw.csv
-https://wdlstorageaccount.blob.core.windows.net/continuous/01N04E36Q001M/por/01N04E36Q001M_Groundwater_Level_Below_Ground_Surface_Daily_Mean.csv
-https://wdlstorageaccount.blob.core.windows.net/continuous/01N04E36Q001M/por/01N04E36Q001M_Groundwater_Surface_Elevation_Raw.csv
-https://wdlstorageaccount.blob.core.windows.net/continuous/01N04E36Q001M/por/01N04E36Q001M_Groundwater_Surface_Elevation_Daily_Mean.csv
-https://wdlstorageaccount.blob.core.windows.net/continuous/01N04E36Q001M/por/01N04E36Q001M_Water_Temperature_Raw.csv
-https://wdlstorageaccount.blob.core.windows.net/continuous/01N04E36Q001M/por/01N04E36Q001M_Water_Temperature_Daily_Mean.csv
-
-More recent (B05155) Merced River at Cressey
-https://wdlstorageaccount.blob.core.windows.net/continuous/B05155/por/B05155_Stage_Raw.csv
-https://wdlstorageaccount.blob.core.windows.net/continuous/B05155/por/B05155_Stage_Daily_Mean.csv
-https://wdlstorageaccount.blob.core.windows.net/continuous/B05155/por/B05155_Flow_Raw.csv
-https://wdlstorageaccount.blob.core.windows.net/continuous/B05155/por/B05155_Flow_Daily_Mean.csv
-https://wdlstorageaccount.blob.core.windows.net/continuous/B05155/por/B05155_Water_Temperature_Raw.csv
-https://wdlstorageaccount.blob.core.windows.net/continuous/B05155/por/B05155_Water_Temperature_Daily_Mean.csv
-https://wdlstorageaccount.blob.core.windows.net/continuous/B05155/por/B05155_Electrical_Conductivity_at_25C_Raw.csv
-https://wdlstorageaccount.blob.core.windows.net/continuous/B05155/por/B05155_Electrical_Conductivity_at_25C_Daily_Mean.csv
-
-Site Reports
-https://wdlstorageaccount.blob.core.windows.net/continuousdata/docs/A0079000/POR/Site_Report.txt
-https://wdl.water.ca.gov/StationDetails.aspx?Station=01N04E36Q001M
 """
 # -*- coding: utf-8 -*-
+import datetime as dt
+import io
 from bs4 import BeautifulSoup
 import pandas as pd
-import requests
+from collect.utils import utils
 
 
-STATION_TYPES = ['Groundwater', 'Surface Water', 'Tide Station', 'Water Quality']
-
-
-def get_cawdl_site_report_url(station_number):
+def get_cawdl_site_detail_url(site_id):
     """
+    get the URL for the station detail page with links to timeseries charts and data downloads
+
     Arguments:
-        station_number (str): string CAWDL well or station identifier
+        site_id (str): string representing CAWDL well or station identifier; ie '01N04E36Q001M', 'B05155'
+    Returns:
+        url (str): the URL for site detail page
+    """
+    return f'https://wdl.water.ca.gov/StationDetails.aspx?Station={site_id}'
+
+
+def get_cawdl_site_report_url(site_id):
+    """
+    get the URL for the station site report, which is a text-format detail page
+
+    Arguments:
+        site_id (str): string representing CAWDL well or station identifier; ie '01N04E36Q001M', 'B05155'
     Returns:
         url (str): the query URL for site report
     """
     url_base = 'https://wdlstorageaccount.blob.core.windows.net/continuous'
-    return f'{url_base}data/docs/{station_number}/POR/Site_Report.txt'
+    return f'{url_base}data/docs/{site_id}/POR/Site_Report.txt'
 
 
-def get_cawdl_continuous_data_url(station_number, parameter, output_interval):
+def get_cawdl_continuous_data_url(site_id, parameter, output_interval):
     """
+    get the URL for the station period of record, continuous data timeseries for a parameter and output interval
+
     Arguments:
-        station_number (str): string CAWDL well or station identifier
+        site_id (str): string representing CAWDL well or station identifier; ie '01N04E36Q001M', 'B05155'
         parameter (str): the quantity being recorded
         output_interval (str): raw or the operation like daily mean, min/max, etc.
     Returns:
@@ -86,63 +72,52 @@ def get_cawdl_continuous_data_url(station_number, parameter, output_interval):
     if output_interval not in ['Raw', 'Daily_Mean', 'Daily_Min_Max']:
         raise ValueError(f'ERROR: invalid `output_interval`: {output_interval}')
 
-    return f'{url_base}/{station_number}/por/{station_number}_{parameter}_{output_interval}.csv'
+    return f'{url_base}/{site_id}/por/{site_id}_{parameter}_{output_interval}.csv'
 
 
-def get_cawdl_data(site_id):
+def get_cawdl_site_detail(site_id):
     """
-    download well timeseries data from CAWDL database
+    parse HTML file structure; extract station/well metadata for continuous data sites
 
     Arguments:
-        site_id (str): string representing CAWDL site id; ie '17202'
+        site_id (str): string representing CAWDL well or station identifier; ie '01N04E36Q001M', 'B05155'
     Returns:
-        dictionary: dictionary of 'data' and 'info' with dataframe of timeseries and station metadata
+        info (dict): dictionary of site info extracted from detail page
     """
-    raise NotImplementedError
-    cawdl_url = 'http://wdl.water.ca.gov/waterdatalibrary/groundwater/hydrographs/'
-    table_url = cawdl_url + 'report_xcl_brr.cfm?CFGRIDKEY={0}&amp;type=xcl'.format(site_id)
+    url = get_cawdl_site_detail_url(site_id)
+    soup = BeautifulSoup(utils.get_session_response(url).content, 'html5lib')
 
-    # read historical ground water timeseries from "recent groundwater level data" tab
-    df = pd.read_csv(table_url, header=2, skiprows=[1], parse_dates=[0], index_col=0)
-    return {'data': df, 'info': get_cawdl_well_detail(site_id)}
+    # parse the station detail table
+    station_table = soup.find_all('table', {'class': 'OSFillParent'})[0]
+    station_df = pd.read_html(f'<div>{str(station_table)}</div>', flavor='bs4', index_col=0)[0]
+    station_df.index = station_df.index.str.rstrip(' :')
+    info = station_df[1].to_dict()
+
+    # parse the data availability table
+    data_table = soup.find_all('table', {'class': 'OSFillParent'})[1]
+    data_df = pd.read_html(f'<div>{str(data_table)}</div>', flavor='bs4')[0]
+    data_df['Parameter'] = data_df['Parameter'].fillna(method='ffill')
+
+    # add data availability table and site report URL to site info dictionary
+    info.update({'url': url,
+                 'availability': data_df,
+                 'report': get_cawdl_site_report_url(site_id)})
+
+    # return the site data as a dictionary
+    return info
 
 
-def get_cawdl_well_detail(site_id):
+def get_cawdl_continuous_data(site_id, parameter, output_interval, start=None, end=None):
     """
-    parse HTML file structure; extract station/well metadata
+    download timeseries data from CAWDL database with optional start and end filter
 
     Arguments:
-        site_id (str): string representing CAWDL site id; ie '17202'
-    Returns:
-        dictionary: dictionary of well metadata extracted from detail page
-    """
-    raise NotImplementedError
-    cawdl_url = 'http://wdl.water.ca.gov/waterdatalibrary/groundwater/hydrographs/'
-    site_url = cawdl_url + 'brr_hydro.cfm?CFGRIDKEY={0}'.format(site_id)
-
-    well_info = {}
-    soup = BeautifulSoup(requests.get(site_url).content, 'html5lib')
-    for table in soup.find_all('table')[1:]:
-        for tr in table.find_all('tr'):
-            cells = tr.find_all('td')
-            if len(cells) == 2:
-                key = cells[0].text.strip()
-                try:
-                    value = float(cells[1].text.strip())
-                except ValueError:
-                    value = cells[1].text.strip()
-                well_info.update({key: value})
-
-
-def get_cawdl_continuous_data(site_id, parameter, output_interval):
-    """
-    download timeseries data from CAWDL database
-
-    Arguments:
-        site_id (str): string representing cawdl site id; ie 'B94100'
+        site_id (str): string representing CAWDL well or station identifier; ie '01N04E36Q001M', 'B05155'
         variable (str): measurement description; ie 'STAGE' or 'FLOW' or 'CONDUCTIVITY' or 'WATER_TEMPERATURE'
         interval (str): measurement time interval; ie '15-MINUTE_DATA' or 'DAILY_MEAN' or 'DAILY_MINMAX' or
             'POINT' (default for conductivity & temp)
+        start (datetime.datetime): optional start time to filter timeseries data record
+        end (datetime.datetime): optional start time to filter timeseries data record
     Returns:
         dictionary: dictionary of 'data' and 'info' with dataframe of timeseries and station metadata
     """
@@ -151,6 +126,13 @@ def get_cawdl_continuous_data(site_id, parameter, output_interval):
     # read period of record timeseries data and rename index
     df = pd.read_csv(url, header=0, skiprows=2, parse_dates=[0], index_col=0, usecols=[0, 1, 2])
     df.index.name = 'Time and Date'
+
+    # time window is specified
+    if isinstance(start, dt.datetime):
+        df = df.truncate(before=start)
+
+    if isinstance(end, dt.datetime):
+        df = df.truncate(after=end)
 
     # extract variable labels and data qualifier definitions
     record_info = pd.read_csv(url, skiprows=2, index_col=None, usecols=[3]).iloc[:, 0].dropna().tolist()
@@ -163,49 +145,129 @@ def get_cawdl_continuous_data(site_id, parameter, output_interval):
             'info': {'series_metadata': series_info, **get_cawdl_continuous_data_site_report(site_id)['info']}}
 
 
-def get_cawdl_continuous_data_site_report(site_id):
+def get_cawdl_continuous_data_site_report(site_id, include_raw=False):
     """
-    download site report from CAWDL database
+    download CAWDL database site report for continuous data stations by site_id
 
     Arguments:
-        site_id (str): string representing cawdl site id; ie 'B94100'
+        site_id (str): string representing CAWDL well or station identifier; ie '01N04E36Q001M', 'B05155'
+        include_raw (bool): flag for whether to include the raw site report text
     Returns:
         dictionary: 'info' key with text from CAWDL site report
     """
-    report_url = get_cawdl_site_report_url(site_id)
+    url = get_cawdl_site_report_url(site_id)
+
+    # metadata dictionary
+    info = {'url': url}
 
     # parse HTML file structure; extract station/well metadata
-    site_info = {}
-    file = requests.get(report_url)
+    response = utils.get_session_response(url).text
+    lines = response.split('\r\n')
 
-    site_info['available series'] = []
-    start_index = 9999
-    end_index = 9999
+    # add the raw page text if specified
+    if include_raw:
+        info.update({'raw': response})
 
-    for i, line in enumerate(file):
-        if i > 5: # skip title lines
-            try:
-                decoded_line = line.decode().strip()
+    # break on these section titles to separate text-formatted tables
+    section_titles = ['DATUM SHIFTS',
+                      'GAUGINGS',
+                      'PERIOD OF RECORD (ARCHIVE)',
+                      'RATING TABLES',
+                      'SITE DESCRIPTION',
+                      'STATION DESCRIPTION',
+                      'TIME SHIFTS',
+                      'TIME-BASED TABLES']
 
-                if 'Comment' in decoded_line:
-                    end_index = i
+    # separate site report into sections for processing
+    sections = {}
+    header = None
+    for line in lines:
+        clean = line.strip()
+        if clean.endswith(' Page 1'):
+            info['published'] = dt.datetime.strptime(clean.split('Output')[1].strip(), '%m/%d/%Y Page 1').date()
+        if clean in section_titles and clean not in sections:
+            header = clean
+            sections.update({header: []})
+        elif bool(clean) and header is not None:
+            if (clean.startswith('California Department of Water Resources')
+                    or clean.startswith('HYSITREP')
+                    or clean.startswith(site_id)):
+                continue
+            sections[header].append(line)
 
-                if 'Variable' in decoded_line:
-                    start_index = i
+    def _parse_description(lines):
+        ret = {}
+        for line in lines:
+            key, value = line.split(':', 1)
+            ret[key] = value.strip()
+        return ret
 
-                if i <= end_index:
-                    if ':' in decoded_line:
-                        key = decoded_line.split(':')[0]
-                        value = decoded_line.split(':')[1]
-                        value =  '\n'.join([s for s in value.splitlines() if s]).strip()
-                        site_info.update({key: value})
+    def _parse_table(lines, **kwargs):
+        return pd.read_fwf(io.StringIO('\n'.join(lines)), **kwargs)
 
-                if i > start_index:
-                    series = decoded_line.split('  ')[0]
-                    if decoded_line != '':
-                        site_info['available series'].append(series)
+    if 'SITE DESCRIPTION' in sections:
+        info.update({'site_description': _parse_description(sections['SITE DESCRIPTION'])})
 
-            except UnicodeDecodeError:
-                pass
+    if 'STATION DESCRIPTION' in sections:
+        info.update({'station_description': _parse_description(sections['STATION DESCRIPTION'])})
 
-    return {'info': site_info}
+    if 'DATUM SHIFTS' in sections:
+        info.update({'datum_shifts': _parse_table(sections['DATUM SHIFTS'])})
+
+    if 'GAUGINGS' in sections:
+        info.update({'gaugings': [x.strip() for x in sections['GAUGINGS']]})
+
+    if 'PERIOD OF RECORD (ARCHIVE)' in sections:
+        info.update({'period_of_record_archive': _parse_table(sections['PERIOD OF RECORD (ARCHIVE)'],
+                                                              header=0,
+                                                              colspecs=[(0, 40), (40, 56), (56, 100)])})
+
+    if 'RATING TABLES' in sections:
+        colspecs = [(0, 32), (32, 60), (60, 71), (71, 80), (80, 100), (100, 200)]
+        info.update({'rating_tables': _parse_table(sections['RATING TABLES'],
+                                                   header=0,
+                                                   skiprows=1,
+                                                   index_col=None,
+                                                   colspecs=colspecs)})
+
+    if 'TIME SHIFTS' in sections:
+        info.update({'time_shifts': _parse_table(sections['TIME SHIFTS'],
+                                                 skiprows=1,
+                                                 names=['Date', 'Time', 'Shift', 'Comment'])})
+
+    if 'TIME-BASED TABLES' in sections:
+        colspecs = [(0, 40), (40, 50), (50, 59), (59, 76), (76, 88), (88, 100)]
+        info.update({'time_based_tables': _parse_table(sections['TIME-BASED TABLES'], header=0, colspecs=colspecs)})
+
+    return {'info': info}
+
+
+def get_cawdl_dataset_overview(dataset):
+    """
+    station info provided in summary tables on the CNRA open data portal
+
+    Arguments:
+        dataset (str): the name of the CSV file
+    Returns:
+        df (pandas.DataFrame): dataframe with contents of specified CSV dataset
+    """
+    if dataset not in ['period_of_record',
+                       'gwl-stations',
+                       'gwl-quality_codes',
+                       'gwl-monthly',
+                       'gwl-daily',
+                       'stations',
+                       'station-trace-download-links']:
+        raise ValueError(f'ERROR: `dataset` {dataset} is not available.')
+
+    resource_path = {
+        'period_of_record': '3f96977e-2597-4baa-8c9b-c433cea0685e/resource/8ff3a841-d843-405a-a360-30c740cc8691',
+        'gwl-stations': '618c73fe-b28c-4399-a824-43d0278fe974/resource/03967113-1556-4100-af2c-b16a4d41b9d0',
+        'gwl-quality_codes': '618c73fe-b28c-4399-a824-43d0278fe974/resource/06437a09-ac72-4d5b-91a7-e5963349b486',
+        'gwl-monthly': '618c73fe-b28c-4399-a824-43d0278fe974/resource/16f256f8-35a4-4cab-ae02-399a2914c282',
+        'gwl-daily': '618c73fe-b28c-4399-a824-43d0278fe974/resource/84e02633-00ca-47e8-97ec-c0093313ddcd',
+        'stations': 'fcba3a88-a359-4a71-a58c-6b0ff8fdc53f/resource/c2b08f48-acfd-4a5b-9799-0f3e07d83192',
+        'station-trace-download-links': 'fcba3a88-a359-4a71-a58c-6b0ff8fdc53f/resource/cdb5dd35-c344-4969-8ab2-d0e2d6c00821',
+    }.get(dataset)
+
+    return pd.read_csv(f'https://data.cnra.ca.gov/dataset/{resource_path}/download/{dataset}.csv')
