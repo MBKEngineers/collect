6fb98a282f9bcaf20391bcc03bfbbbc31f8dc992
Carly Narlesky <narlesky@mbkengineers.com>
2024-11-27 10:47:42 -0800

    Update USACE tests for compat with implemented trimming of get_wcds_data to start and end times

--- collect/tests/test_usace.py
+++ collect/tests/test_usace.py
@@ -35,8 +35,8 @@ class TestUSACE(unittest.TestCase):
 
     def test_get_data(self):
         result = wcds.get_wcds_data('sha', dt.datetime(2023, 1, 15), dt.datetime(2023, 2, 1), interval='d')
-        self.assertEqual(result['data'].shape, (398, 16))
-        self.assertEqual(result['data']['Storage'].tolist()[:4], [1592122.0, 1590203.0, 1585627.0, 1582232.0])
+        self.assertEqual(result['data'].shape, (18, 16))
+        self.assertEqual(result['data']['Storage'].tolist()[:4], [2235532.0, 2308907.0, 2357517.0, 2392661.0])
 
     def test_get_wcds_reservoirs(self):
         """
@@ -46,8 +46,8 @@ class TestUSACE(unittest.TestCase):
 
     def test_get_wcds_data(self):
         result = wcds.get_wcds_data('sha', dt.datetime(2023, 1, 15), dt.datetime(2023, 2, 1), interval='d')
-        self.assertEqual(result['data'].shape, (398, 16))
-        self.assertEqual(result['data']['Storage'].tolist()[:4], [1592122.0, 1590203.0, 1585627.0, 1582232.0])
+        self.assertEqual(result['data'].shape, (18, 16))
+        self.assertEqual(result['data']['Storage'].tolist()[:4], [2235532.0, 2308907.0, 2357517.0, 2392661.0])
 
     def test_get_release_report(self):
         self.assertEqual(wcds.get_release_report('buc')['info']['units'], 'cfs')
